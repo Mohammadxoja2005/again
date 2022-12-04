08fd23fbfda13ff04f3f2bd7e7c3cde9caed6ef9
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2022-12-04 23:39:26 +0500

    cdsalkjlas

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 // db.sequelize.sync().then(() => {
-  app.listen(4000, () => {
+  app.listen(process.env.PORT || 4000, () => {
     console.log("server started...");
   });
 // });
